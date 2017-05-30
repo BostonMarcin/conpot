5a564dee945a1cf988b017f412b52f70ff77daa1
marcinp <marcin170288@gmail.com>
2017-05-30 09:37:37 +0200

    improved function extra

--- conpot/core/loggers/utils.py
+++ conpot/core/loggers/utils.py
@@ -1,14 +1,23 @@
 
 def extra(_locals, *args, **kwargs):
 
+    _extra_str = []
+
     VARS = {'client_ip':('addr',
                          'sockaddr',
                          )}
 
-    #return client's address
-    for _ in VARS['client_ip']:
-        if _ in _locals: return _locals[_]
 
-    return 'Undefined'
+    #build extra for client's address
+    for _ in VARS['client_ip']:
+        _str = "client_ip : Undefined"
+        if _ in _locals:
+            _str = "client_ip : {n}".format(n=_locals['client_ip'])
+            break
+    _extra_str.append(_str)
+
+    return ' '.join(_extra_str)
+
+
 
 
