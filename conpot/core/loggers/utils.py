b99f3325f26021eb1c03c85780b3bdb006a7de4c
marcinp <marcin170288@gmail.com>
2017-05-30 10:19:49 +0200

    Fixed extra, it must return dict.

--- conpot/core/loggers/utils.py
+++ conpot/core/loggers/utils.py
@@ -9,17 +9,17 @@ VARS = {'client_ip':('addr',
 
 def extra(_locals, *args, **kwargs):
 
-    _extra_str.append('hostname : {n}'.format(n = VARS['hostname']))
+    _extra = {}
+    _extra['hostname'] =  VARS['hostname']
 
     #build extra for client's address
+    _extra['client_ip'] = "Undefined"
     for _ in VARS['client_ip']:
-        _str = "client_ip : Undefined"
         if _ in _locals:
-            _str = "client_ip : {n}".format(n=_locals['client_ip'])
+            _extra['client_ip'] =_locals[_]
             break
-    _extra_str.append(_str)
 
-    return ' '.join(_extra_str)
+    return _extra
 
 
 
